118264fc71593ceea397db58e48356f1855b049a
Anil Kumar <142671046+aniljasel@users.noreply.github.com>
2025-12-12 02:47:39 +0530

    Updated server/utils/sendEmail.js

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,10 +2,15 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
-        service: 'gmail', // Or use host/port from env
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
+        },
+        tls: {
+            rejectUnauthorized: false
         }
     });
 
