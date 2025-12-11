912a98c965b3cd1974c0621a97920974106a0701
Anil Kumar <142671046+aniljasel@users.noreply.github.com>
2025-12-12 02:57:11 +0530

    Debugging SMTP Connection

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,28 +1,24 @@
 const nodemailer = require('nodemailer');
 
-const sendEmail = async (options) => {
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
 
-    const mailOptions = {
-        from: `Productr <${process.env.EMAIL_USER}>`,
-        to: options.email,
-        subject: options.subject,
-        text: options.message,
-        html: options.html
-    };
+const mailOptions = {
+    from: `Productr <${process.env.EMAIL_USER}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    html: options.html
+};
 
-    await transporter.sendMail(mailOptions);
+await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendEmail;
