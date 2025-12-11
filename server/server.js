1ddd3f3b1fdf9627b3024ace6cf8a8229ab0ce34
Anil Kumar <142671046+aniljasel@users.noreply.github.com>
2025-12-12 03:38:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*', // Allow all origins for debug
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(helmet());
 app.use(express.json({ limit: '200mb' }));
 app.use(express.urlencoded({ limit: '200mb', extended: true }));
