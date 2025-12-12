2dcbfeee075834fee57b1e254ff5ab55d6ac211b
Anil Kumar <142671046+aniljasel@users.noreply.github.com>
2025-12-12 15:09:04 +0530

    Update sendEmail.js

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer');
 
+const sendEmail = async (options) => {
     // Debug Configuration
     console.log(`[Email] Attempting to send to: ${options.email}`);
     console.log(`[Email] Host: ${process.env.SMTP_HOST}`);
@@ -9,7 +10,7 @@ const nodemailer = require('nodemailer');
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: Number(process.env.SMTP_PORT),
-        secure: false, 
+        secure: false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
