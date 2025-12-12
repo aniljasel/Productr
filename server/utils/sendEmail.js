7b462ce6ad4535fe73e8fbb47eb749a0d775b781
Anil Kumar <142671046+aniljasel@users.noreply.github.com>
2025-12-12 12:22:09 +0530

    Fixed Syntax Error

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,31 +1,32 @@
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    },
-    tls: {
-        rejectUnauthorized: false
-    },
-    family: 4, // Force IPv4
-    logger: true,
-    debug: true,
-    connectionTimeout: 10000,
-    greetingTimeout: 5000,
-    socketTimeout: 10000
-});
+const sendEmail = async (options) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        },
+        tls: {
+            rejectUnauthorized: false
+        },
+        family: 4, // Force IPv4
+        logger: true,
+        debug: true,
+        connectionTimeout: 10000,
+        greetingTimeout: 5000,
+        socketTimeout: 10000
+    });
 
-const mailOptions = {
-    from: `Productr <${process.env.EMAIL_USER}>`,
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    html: options.html
-};
+    const mailOptions = {
+        from: `Productr <${process.env.EMAIL_USER}>`,
+        to: options.email,
+        subject: options.subject,
+        text: options.message,
+        html: options.html
+    };
 
-await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendEmail;
